3039bb285acf93070354e6cdbb4b729dc28a51cb
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-08-06 00:23:11 +0430

    20 minutes of session 4

--- src/components/MyContent.vue
+++ src/components/MyContent.vue
@@ -7,20 +7,24 @@
 
       <b-list-group>
         <b-list-group-item
-          v-for="(answersList, i) in answersMethod"
+          v-for="(answersList, i) in shuffledAnswes"
           :key="i"
           @click="changeIndex(i)"
+          :class="[cIndex === i ? 'selected' : '']"
         >
           {{ answersList }}
         </b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="primary" href="#" @click="submitAnswer"
+        >Submit</b-button
+      >
       <b-button variant="success" href="#" @click="next">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 <script>
+import _ from "lodash";
 export default {
   props: {
     cQuestion: Object,
@@ -29,13 +33,36 @@ export default {
   data() {
     return {
       cIndex: null,
+      shuffledAnswes: [],
+      correctAnswer: null,
     };
+  },
+  watch: {
+    cQuestion: {
+      immediate: true,
+      handler() {
+        this.cIndex = null;
+        this.shuffleAnswers();
+      },
+    },
   },
   methods: {
     changeIndex(i) {
       this.cIndex = i;
       console.log(i);
     },
+    shuffleAnswers() {
+      let answers = [
+        ...this.cQuestion.incorrect_answers,
+        this.cQuestion.correct_answer,
+      ];
+      this.shuffledAnswes = _.shuffle(answers);
+      // this.correctAnswer = this.cQuestion.correct_answer;
+      this.correctAnswer = this.shuffledAnswes.indexOf(
+        this.cQuestion.correct_answer
+      );
+    },
+    submitAnswer() {},
   },
   computed: {
     answersMethod() {
@@ -54,5 +81,17 @@ export default {
 }
 .btn {
   margin: 10px 2px;
+}
+
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.incorrect {
+  background-color: red;
 }
 </style>
