c0175f68347b19a852bb0a5061b1457ef2ae118b
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-08-11 20:16:12 +0430

    Start of School project for Router and VueX

--- MoviesQuiz/README.md
+++ MoviesQuiz/README.md
@@ -1,4 +1,4 @@
-# school
+# vue-cli
 
 ## Project setup
 ```
---
+++ MoviesQuiz/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <MyHeader :correctCount="correctCount" :totalCount="totalCount" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col SM="6" offset="3">
+          <MyContent
+            v-if="questionList.length"
+            :cQuestion="questionList[index]"
+            :next="onNext"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import MyHeader from "./components/MyHeader.vue";
+import MyContent from "./components/MyContent.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      questionList: [],
+      index: 0,
+      correctCount: 0,
+      totalCount: 0,
+    };
+  },
+  methods: {
+    onNext() {
+      console.log("next");
+      if (this.index < 9) {
+        this.index++;
+      }
+    },
+    increment(is_correct) {
+      if (is_correct) {
+        this.ctCount++;
+      }
+      this.totalCount++;
+    },
+  },
+  components: {
+    MyHeader,
+    MyContent,
+  },
+  mounted: function () {
+    fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple", {
+      method: "GET",
+    })
+      .then((response) => {
+        // console.log(response.json());
+        return response.json();
+      })
+      .then((result) => {
+        this.questionList = result.results;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
